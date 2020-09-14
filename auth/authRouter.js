a76151b1d72df354397e785fd57677a7bb38a563
DeeDowns <dee_downs@outlook.com>
2020-09-14 19:20:03 -0400

    endpoints complete

--- auth/authRouter.js
+++ auth/authRouter.js
@@ -5,7 +5,7 @@ const router = express.Router()
 
 const Users = require('../users/userModel')
 
-router.post('/register',validateUser, (req, res) => {
+router.post('/register', validateUser, (req, res) => {
     const userInfo = req.body
     const rounds = process.env.BCRYPT_ROUND || 4
     const hash = bcrypt.hashSync(userInfo.password, rounds)
@@ -18,17 +18,43 @@ router.post('/register',validateUser, (req, res) => {
     })
     .catch(err => {
         console.log(err)
-        res.status(500).json({ message: 'error registering user'})
+        res.status(500).json({ message: err.message})
     })
    
 })
 
-router.post('/login', (req, res) => {
+router.post('/login', validateUser, (req, res) => {
+    const credentials = req.body
 
+    Users.getBy({ username: credentials.username })
+    .then(([user]) => {
+        console.log(user)
+        if(user && bcrypt.compareSync(credentials.password, user.password)) {
+            req.session.username = user.username
+            res.status(201).json({ message: `${credentials.username} logged in` })
+        } else {
+            res.status(401).json({ message: 'you shall not pass' })
+        }
+    })
+    .catch(err => {
+        console.log(error)
+        res.status(500).json({ message: err.message })
+
+    })
 })
 
 router.get('/logout', (req, res) => {
-
+    if(req.session) {
+        req.session.destroy(err => {
+            if(err) {
+                res.status(500).json({ message: `error loggin out ${err.message}` })
+            } else {
+                res.status(200).json({ message: 'logged out'})
+            } 
+        })
+    } else {
+        res.status(204).end()
+    }
 })
 
 function validateUser(req, res, next) {
@@ -40,9 +66,11 @@ function validateUser(req, res, next) {
     }
 }
 
-function validateCredentials(req, res, next) {
+// function validateCredentials(req, res, next) {
+//     const {username, password } = req.body 
+//     if(username)
 
-}
+// }
 
 module.exports = router
 
